cf0f5f6d52d0f85950222eee6227c882cc09d24f
Hik <luizhenrique98@gmail.com>
2020-08-30 18:55:26 -0300

    Add needs

---
+++ Assets/MainScene/Scripts/NeedsController.cs
@@ -0,0 +1,54 @@
+﻿using DefaultNamespace;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeedsController : MonoBehaviour
+{
+    [SerializeField] ChickController _chickController;
+    [SerializeField] float initialValue;
+    [SerializeField] float decaySpeed;
+    [SerializeField] float recoverySpeed;
+
+    Dictionary<ChickState, float> _needs = new Dictionary<ChickState, float>();
+
+    public float GetNeedValue(ChickState need)
+    {
+        return _needs[need];
+    }
+
+    private void Awake()
+    {
+        foreach (ChickState state in Enum.GetValues(typeof(ChickState)))
+        {
+            if (state != ChickState.Idle)
+            {
+                _needs[state] = initialValue;
+            }
+        }
+    }
+
+    private void Update()
+    {
+        foreach (ChickState state in Enum.GetValues(typeof(ChickState)))
+        {
+            if (state == ChickState.Idle) continue;
+
+            if (state == _chickController.ChickState)
+            {
+                UpdateNeed(state, recoverySpeed * Time.deltaTime);
+            }
+            else
+            {
+                UpdateNeed(state, -decaySpeed * Time.deltaTime);
+            }
+        }
+    }
+
+    private void UpdateNeed(ChickState state, float delta)
+    {
+        _needs[state] += delta;
+        _needs[state] = Mathf.Clamp01(_needs[state]);
+    }
+}
---
+++ Assets/MainScene/Scripts/NeedsDisplay.cs
@@ -0,0 +1,20 @@
+﻿using DefaultNamespace;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NeedsDisplay : MonoBehaviour
+{
+    [SerializeField] NeedsController _needsController;
+    [SerializeField] Slider _exerciseDisplay;
+    [SerializeField] Slider _waterDisplay;
+    [SerializeField] Slider _foodDisplay;
+
+    private void Update()
+    {
+        _exerciseDisplay.value = _needsController.GetNeedValue(ChickState.Exercising);
+        _waterDisplay.value = _needsController.GetNeedValue(ChickState.DrinkingWater);
+        _foodDisplay.value = _needsController.GetNeedValue(ChickState.Eating);
+    }
+}
